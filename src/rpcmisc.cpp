5cf8fcc96980cd0cd5e6a16c193e46d4fc79b5cb
carebitcoin <carebitcoin@gmail.com>
2019-04-10 02:54:06 +0200

    Fix segfault cli getinfo while loading block index

--- src/rpcmisc.cpp
+++ src/rpcmisc.cpp
@@ -108,6 +108,11 @@ Value getinfo(const Array& params, bool fHelp)
     obj.push_back(Pair("proxy", (proxy.IsValid() ? proxy.proxy.ToStringIPPort() : string())));
     obj.push_back(Pair("difficulty", (double)GetDifficulty()));
     obj.push_back(Pair("testnet", Params().TestnetToBeDeprecatedFieldRPC()));
+    // During inital block verification chainActive.Tip() might be not yet initialized
+    if (chainActive.Tip() == NULL) {
+        obj.push_back(Pair("status", "Blockchain information not yet available"));
+        return obj;
+    }
     obj.push_back(Pair("moneysupply",ValueFromAmount(chainActive.Tip()->nMoneySupply)));
     Object zcarebitcoinObj;
     for (auto denom : libzerocoin::zerocoinDenomList) {
@@ -115,7 +120,7 @@ Value getinfo(const Array& params, bool fHelp)
     }
     zcarebitcoinObj.emplace_back(Pair("total", ValueFromAmount(chainActive.Tip()->GetZerocoinSupply())));
     obj.emplace_back(Pair("zCAREsupply", zcarebitcoinObj));
-    
+
 #ifdef ENABLE_WALLET
     if (pwalletMain) {
         obj.push_back(Pair("keypoololdest", pwalletMain->GetOldestKeyPoolTime()));
